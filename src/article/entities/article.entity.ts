91408e24dc0bdb817e68d00167777dbb456a71e7
amir2015 <zahranovich2@gmail.com>
2025-02-18 19:41:06 +0200

    article entity

---
+++ src/article/entities/article.entity.ts
@@ -0,0 +1,26 @@
+import { BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  title: string;
+  @Column()
+  description: string;
+  @Column()
+  body: string;
+  @Column({ default: 0 })
+  favoritesCount: number;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+  @Column()
+  slug: string;
+  @Column('simple-array')
+  tagList: string[];
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updatedAt = new Date();
+  }
+}
